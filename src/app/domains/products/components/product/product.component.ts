36b55fb0d7ca4467d38aaad0556f6d2a07e6bc14
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2025-05-17 18:34:25 -0500

    Add date-fns library and implement new pipes for date formatting
    - Added date-fns as a dependency for date manipulation.
    - Introduced ReversePipe for reversing strings in templates.
    - Created TimeAgoPipe to format dates into a human-readable "time ago" format.
    - Updated ProductComponent to include ReversePipe in imports.
    - Cleaned up product.model.ts by removing commented-out code.

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { CommonModule } from '@angular/common';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
 @Component({
   selector: 'app-product',
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe],
   templateUrl: './product.component.html',
 })
 export class ProductComponent {
